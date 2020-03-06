47032dca36609e39ce2b9a9c5c9b6b74bb566433
Azshian <aliceralde@egi.utah.edu>
2020-03-05 17:46:23 -0700

    added random message

---
+++ Data Analysis/Tests/import numpy as np.py
@@ -0,0 +1,14 @@
+"""
+Contains tests to see if pytest is working properly.
+
+This file is a part of Jake's C Compiler (JCC)
+(c) Copyright 2019 Jacob Larkin
+"""
+
+import numpy as np
+
+LIST_A = np.array([1, 2, 3])
+listB = 3
+listC = listA*listB
+print(listC)
+
