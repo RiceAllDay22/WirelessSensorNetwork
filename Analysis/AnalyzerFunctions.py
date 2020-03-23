269e0539ba22630dab677c75d75f823c5e0b0231
Azshian <aliceralde@egi.utah.edu>
2020-03-22 21:35:03 -0600

    Analyzer optimizing

--- Analysis/AnalyzerFunctions.py
+++ Analysis/AnalyzerFunctions.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.dates import DateFormatter
 
 
 def importCheck(rawAllDataNP, rawAllDataDF):
@@ -87,9 +88,18 @@ def windowSelect(filAllDataNP, filAllDataDF, leftFrame, rightFrame):
         plt.close('all')
         x  = fraAllDataNP[:,3]  
         y  = fraAllDataNP[:,1]
-        plt.plot(x, y, "r-", linewidth = 0.5)
-        #plt.plot(x, y, "k-")
+        fig, ax = plt.subplots()
+        plt.plot_date(x, y, "r-", linewidth = 0.5)
+        
+        #Plot Formatting
+        plt.title('Selected Frame', fontsize = 15)
+        plt.xlabel('Time', fontsize=12)
+        plt.ylabel('CO2 ppm', fontsize=12)
+        formatter = DateFormatter('%H:%M')
+        ax.xaxis.set_major_formatter(formatter)
+        plt.gcf().autofmt_xdate()#plt.plot(x, y, "k-")
         plt.grid()
+        plt.show()
 
     else:
             print('Invalid selection')
@@ -110,3 +120,60 @@ def missingDataDeterminer(data):
         [name] is data])
     return missingData
 
+
+def estimateData(fraAllDataNP, missingFra):  
+    misGas  = np.array([])
+    misReal = np.zeros((len(missingFra)))
+    missingFra = missingFra.astype(int) 
+
+    #Perform estimation for every missing index
+    for j in missingFra:
+        left  = j - 1
+        right = j + 1
+        #Check that bordering points exist
+        if((left in filAllDataNP[:,0])&(right in filAllDataNP[:,0]))==True:
+            leftIndex  = np.where(filAllDataNP[:,0] == left)
+            rightIndex = np.where(filAllDataNP[:,0] == right)
+            leftData   = filAllDataNP[leftIndex[0][0]][1]
+            rightData  = filAllDataNP[rightIndex[0][0]][1]
+            #Check that bordering points are close in value
+            if (abs(leftData - rightData) <= 1) :
+                midData = (leftData + rightData) / 2
+                misGas  = np.append(misGas, midData)
+            else:
+                misGas  = np.append(misGas, None)
+        else:
+            misGas  = np.append(misGas, None)
+    print(missingFra)
+    print(misGas)
+    print(misReal)
+
+    #Combine Estimated Data with Real Data
+    estUnix = np.concatenate((fraAllDataNP[:,0], missingFra), axis = 0)    
+    estGas  = np.concatenate((fraAllDataNP[:,1], misGas),     axis = 0)
+    estReal = np.concatenate((fraAllDataNP[:,2], misReal),    axis = 0)
+    df = pd.DataFrame(np.column_stack((estUnix, estGas, estReal)))
+    df.columns     = ['UnixTime', 'GasData', 'RealData']
+
+    dfs = df.sort_values(by = ['UnixTime'], inplace = False)
+    dfs = dfs.reset_index(drop=True)
+    dfs['DateTime'] = pd.to_datetime(dfs.iloc[:,0], unit = 's')
+    dfs.GasData = dfs.GasData.astype(float)
+    dfs.RealData = dfs.RealData.astype(int)
+
+    #Drop Zeros and Null
+    #dfs = dfs[dfs.GasData != 0.0]
+    #dfs = dfs[dfs.GasData.notnull()]
+
+    estAllDataDF = dfs
+    estAllDataNP = estAllDataDF.to_numpy()
+    nanCounter = estAllDataDF.GasData.isna().sum()
+    return estAllDataNP, estAllDataDF, nanCounter
+
+
+def averageData(estAllDataDF, interval):
+    avgAllDataDF=estAllDataDF.groupby(pd.Grouper(key='DateTime',freq=interval)).mean().dropna()
+    avgAllDataDF = avgAllDataDF.drop(columns=['RealData'])
+    avgAllDataDF.reset_index(level=0, inplace=True)
+    avgAllDataNP = avgAllDataDF.to_numpy()
+    return avgAllDataNP, avgAllDataDF
