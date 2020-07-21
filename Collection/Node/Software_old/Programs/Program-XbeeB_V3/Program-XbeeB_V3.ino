dee5bb5e9ce0de57f7267c1a88221e4adadd91a0
Azshian <aliceralde@egi.utah.edu>
2020-07-21 13:04:53 -0600

    Check

--- Collection/Node/Software_old/Programs/Program-XbeeB_V3/Program-XbeeB_V3.ino
+++ Collection/Node/Software_old/Programs/Program-XbeeB_V3/Program-XbeeB_V3.ino
@@ -13,27 +13,15 @@ void setup() {
   Serial.println("Receiver");
   pinMode(2, INPUT);
   pinMode(3, OUTPUT);
-  pinMode(WIRE_PIN, INPUT_PULLUP);
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
   XBee.begin(9600);
   Serial.println("Flying is for droids.");
 }
 
  
 void loop() {
-  if (digitalRead(WIRE_PIN)) value = 49;
-  else value = 50;
-  
-  if (digitalRead(BUTTON_PIN) == LOW) {
-    XBee.write(value);
-    Serial.println(value);
-    delay(200);
-  }
-  
   while (XBee.available()){
     digitalWrite(LED_PIN, HIGH);
     char data = XBee.read();
     Serial.print(data);
   }
-  digitalWrite(LED_PIN, LOW);
 }
