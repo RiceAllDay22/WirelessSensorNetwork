3182a9aa5446e2084045846dd22396859312d180
Azshian <aliceralde@egi.utah.edu>
2021-01-31 10:09:25 -0700

    File Reader

---
+++ Analysis/Collection/Node/Software_old/ModuleExamples/ModEx-Davis/ModEx-Davis.ino
@@ -0,0 +1,53 @@
+int VaneValue;
+int Direction;
+int CalDirection;
+int LastValue;
+
+int WindSensorPin = 2;
+volatile unsigned long Rotations;
+volatile unsigned long ContactBounceTime;
+
+float WindSpeed;
+#define Offset 0;
+
+void setup() {
+  LastValue = 1;
+  Serial.begin(9600);
+  Serial.println("Begin");
+  pinMode(WindSensorPin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(WindSensorPin), isr_rotation, FALLING);
+   
+}
+
+void loop() {
+  //Wind Direction
+  VaneValue = analogRead(A3);
+  Direction = map(VaneValue, 0, 1023, 0, 360);
+  CalDirection = Direction + Offset;
+
+  if (CalDirection > 360)
+    CalDirection = CalDirection - 360;
+  if (CalDirection < 0)
+    CalDirection = CalDirection + 360;
+
+  //Wind Speed
+  Rotations = 0;
+  sei();
+  delay(1000);
+  cli();
+
+  WindSpeed = Rotations*0.75;
+
+  //Results
+  Serial.print(CalDirection);
+  Serial.print(';');
+  Serial.println(WindSpeed);
+
+}
+
+void isr_rotation () {
+  if ((millis() - ContactBounceTime) > 15 ) {
+    Rotations ++;
+    ContactBounceTime = millis();
+  }
+}
