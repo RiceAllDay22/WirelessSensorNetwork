ce9459b90101bd616cd86fa06a469f9182f790e4
Azshian <aliceralde@egi.utah.edu>
2021-06-29 16:08:32 -0600

    Continuous Beta 11

--- Analysis/Collection/Node/Software_old/Programs/Program-ContinuousBeta11/Program-ContinuousBeta11.ino
+++ Analysis/Collection/Node/Software_old/Programs/Program-ContinuousBeta11/Program-ContinuousBeta11.ino
@@ -4,34 +4,30 @@
 #include <RTClib.h>
 #include <SdFat.h>
 #include <SparkFun_SCD30_Arduino_Library.h> 
-//#include <LowPower.h>
-//#include <LiquidCrystal_I2C.h>
-//#include <SPI.h>
 
 
 //---------------VARIABLES---------------//
 //---------------------------------------//
-SdFat sd;
-SdFile file;
-RTC_DS3231 rtc;
-DateTime dt;
-SCD30 airSensor;
-//LiquidCrystal_I2C lcd(0x27,16,2); // set the LCD address to 0x27 for a 16 chars and 2 line display
+SdFat       sd;
+SdFile      file;
+RTC_DS3231  rtc;
+DateTime    dt;
+DateTime    filestart;
+SCD30       airSensor;
 
 const byte sdChipSelect = SS;
 const byte WSPEED_PIN   = 2;
 const byte DETACH_PIN   = 5;
 const byte LED_PIN      = 6;
-//const byte LCD_PIN    = 4;
 
 const int arrayLength = 5;
-uint32_t unixTime[arrayLength];
-uint8_t  windCyc[arrayLength];
-uint16_t windDir[arrayLength];
-uint16_t concData[arrayLength];
-uint8_t  tempData[arrayLength];
-uint16_t scdData1;
-uint16_t scdData2;
+uint32_t  unixTime[arrayLength];
+uint8_t   windCyc[arrayLength];
+uint16_t  windDir[arrayLength];
+uint16_t  concData[arrayLength];
+uint8_t   tempData[arrayLength];
+uint16_t  scdData1;
+uint16_t  scdData2;
 
 bool wroteNewFile = true;
 volatile byte windClicks  = 0;
@@ -49,41 +45,40 @@ int CalDirection;
 void setup() {
   Serial.begin(9600);
   Serial.println("Setup Begin");
+  
   Wire.begin();
   pinMode(LED_PIN,      OUTPUT);
   pinMode(WSPEED_PIN,   INPUT);
   pinMode(DETACH_PIN,   INPUT_PULLUP); 
   digitalWrite(LED_PIN, HIGH);
-  //pinMode(LCD_PIN,    INPUT_PULLUP);
-  //lcd.init(); lcd.backlight(); lcd.print("Ready"); delay(1000);
-  //lcd.setCursor(0,0); lcd.clear();
 
-  RTCBegin();      delay(2000);
+  RTCBegin();
   //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   //rtc.adjust(DateTime(2020, 6, 3, 23, 7, 0));
   
-  SCD30Begin();    delay(2000);
+  SCD30Begin();
   airSensor.setMeasurementInterval(2);                // # seconds between readings
   airSensor.setAltitudeCompensation(30);              // # meter above sea level
   
-  SDBegin();       delay(2000);
+  SDBegin();
   dt = rtc.now();
-  CreateNewFile(); delay(2000);
+  filestart = dt;
+  CreateNewFile();
+  delay(2000);
   
   attachInterrupt(digitalPinToInterrupt(WSPEED_PIN), isr_rotation, FALLING);
   interrupts();
-  //lcd.clear();
-  //lcd.setCursor(0,0);
-  
+ 
   digitalWrite(LED_PIN, LOW);
   Serial.println("Setup Complete");
 }
 
 
-//---------------MAIN LOOP--------------------//
+//--------------MAIN LOOP----------------//
 //---------------------------------------//
 void loop() {
   DateTime now_dt = rtc.now();
+  
   for (int i=0; i<arrayLength; i++) {
     dt           = now_dt;
     unixTime[i]  = dt.unixtime();
@@ -98,27 +93,18 @@ void loop() {
     
     windCyc[i] = windClicks;
     windClicks = 0;
-    //Serial.println("Data Collected");
   }
-  WriteSample();
-
-
-
-
-//  if (dt.minute() == 0  && dt.second() == 0) {
-//    file.close();
-//    CreateNewFile();
-//  }
-
   
-  //if (digitalRead(LCD_PIN) == HIGH) lcd.backlight();
-  //else lcd.noBacklight();
-  //lcd.print(timeUnix);lcd.setCursor(0,1);lcd.print(windDir);lcd.print(',');lcd.print(gasData);lcd.print(',');lcd.print(totalClicks);
-  //lcd.clear();
-  //lcd.setCursor(0,0);
+  WriteSample();
+  
+  if (filestart.unixtime()+ 3600 <= dt.unixtime()) {
+    filestart = dt;
+    file.close();
+    CreateNewFile();
+  }
 }
 
-//---------------FILE HANDLING---------------//
+//-------------FILE HANDLING-------------//
 //---------------------------------------//
 void CreateNewFile() {
   if (digitalRead(DETACH_PIN)) {
@@ -142,7 +128,7 @@ void WriteSample() {
 
   else {
     digitalWrite(LED_PIN, HIGH);
-    bool binary = false;
+    bool binary = true;
   
     if (binary == false) {
       String line[arrayLength];
@@ -153,28 +139,34 @@ void WriteSample() {
       }
     }
     else {
+      String line[arrayLength];
       for (int j=0; j<arrayLength; j++) {
         file.write((unixTime[j] >> 24) & 0xFF);
         file.write((unixTime[j] >> 16) & 0xFF);
         file.write((unixTime[j] >> 8)  & 0xFF);
         file.write((unixTime[j] >> 0)  & 0xFF);
-        file.write((windCyc[j]>> 0)  & 0xFF);
+        file.write((windCyc[j]  >> 0)  & 0xFF);
         file.write((windDir[j]  >> 8)  & 0xFF);
         file.write((windDir[j]  >> 0)  & 0xFF);
-        file.write((concData[j]  >> 8)  & 0xFF);
-        file.write((concData[j]  >> 0)  & 0xFF);
+        file.write((concData[j] >> 8)  & 0xFF);
+        file.write((concData[j] >> 0)  & 0xFF);
+        file.write((tempData[j] >> 8)  & 0xFF);
+        file.write((tempData[j] >> 0)  & 0xFF);
+
+        line[j] = String(unixTime[j])+","+String(windCyc[j])+","+String(windDir[j])+","+String(concData[j])+","+String(tempData[j]);
+        Serial.println(line[j]);
       }
     }
     file.sync();
     digitalWrite(LED_PIN, LOW);
-    //Serial.println("Sample Written");
   }
 }
 
 
-//---------------FUNCTIONS---------------//
+//--------DATA RETRIEVAL FUNCTIONS-------//
 //---------------------------------------//
-//----------Retrieve Gas Data----------//
+
+//------Retrieve Conc and Temp Data------//
 void CollectGas() {
   if (airSensor.dataAvailable()) {
     scdData1 = airSensor.getCO2();
@@ -194,8 +186,7 @@ void CollectGas() {
 }
 
 
-
-//----------Retrieve Wind Data----------//
+//----------Retrieve Wind Speed----------//
 void isr_rotation() {
   if ((unsigned long)( millis() - lastWindIRQ) > 15 ) {
     lastWindIRQ = millis(); 
@@ -203,6 +194,8 @@ void isr_rotation() {
   }
 }
 
+
+//--------Retrieve Wind Direction-------//
 uint16_t WindDirection() {
   VaneValue    = analogRead(A3);
   Direction    = map(VaneValue, 0, 1023, 0, 360);
@@ -215,23 +208,25 @@ uint16_t WindDirection() {
   return (CalDirection);
 }
 
-//---------------STARTUP MODULES"--------------------//
+
+//----------STARTUP MODULES---------------//
+//---------------------------------------//
+
 //----------RTC Begin----------//
 void RTCBegin() {
   bool success = false;
   while (success == false) {
     if(rtc.begin()) {
       success = true;
-      //lcd.print("RTC OK");
     }
     else {
       Serial.println("RTC Failed");
-      //lcd.print("RTC Failed");
     }
-    delay(1000); //lcd.clear();
+    delay(2000);
   }
   Serial.println("RTC Operational");
 }
+
 
 //----------Gas Begin----------//
 void SCD30Begin() {
@@ -239,16 +234,15 @@ void SCD30Begin() {
   while (success == false) {
     if(airSensor.begin(Wire, false)) {
       success = true;
-      //lcd.print("Sensor OK");
       }      
     else {
       Serial.println("Sensor Failed");
-      //lcd.print("Sensor Failed");
     }
-    delay(1000); //lcd.clear();
+    delay(2000); 
   }
   Serial.println("Sensor Operational");
 }
+
 
 //----------SD Module Begin ----------//
 void SDBegin() {
@@ -256,13 +250,11 @@ void SDBegin() {
   while (success == false) {
     if(sd.begin(sdChipSelect)) {
       success = true; 
-      //lcd.print("SD OK"); 
     }
     else {
       Serial.println("SD Module Failed");
-      //lcd.print("SD Failed");
     }
-    delay(1000); //lcd.clear();
+    delay(2000);
   }
   Serial.println("SD Module Operational");
 }
