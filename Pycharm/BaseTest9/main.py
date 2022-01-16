82fb702fa9c8b157af5cff63800ca648c8d361f2
Azshian <aliceralde@egi.utah.edu>
2022-01-16 12:24:08 -0700

    Check BaseTest9 again

--- Pycharm/BaseTest9/main.py
+++ Pycharm/BaseTest9/main.py
@@ -3,3 +3,4 @@
 print("Hello World!")
 print("BaseTest9 created on Desktop. Take out workspace.xml from the gitignore file")
 print("Works on Laptop 2. Now add some changes")
+print("Works on Desktop. Confirmed back and forth via Github when remove workspace.xml from gitignore file")
